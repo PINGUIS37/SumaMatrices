ac9f86e598873490b4c0a2182faac4cf8ebb17d4
Yael GH <yaelgomezhernandez@gmail.com>
2023-03-08 09:05:01 -0600

    Se creo el programa principal que suma dos matrices

---
+++ src/dev/Suma_de_matrices.java
@@ -0,0 +1,73 @@
+package dev;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Suma_de_matrices {
+	
+	
+	
+	public static void main(String[] args){
+	
+		int i;
+		int j;
+		int n;
+		int m;
+		
+		Scanner input = new  Scanner(System.in);
+		
+		System.out.print("Inserte el número de filas: ");
+		n=input.nextInt();
+		
+		System.out.print("Inserte el número de columnas: ");
+		m=input.nextInt();
+		
+		int M1 [][]= new int[n][m];
+		int M2 [][]= new int[n][m];
+		int M3 [][]= new int[n][m];
+		
+		llenado(M1);
+		llenado(M2);
+		sumar(M1,M2,M3);
+		System.out.println("");
+		imp(M3);
+		
+		input.close();
+	}
+	
+	public static void llenado (int[][] mat1) {
+		
+		Scanner input = new  Scanner(System.in);
+
+		System.out.println("Lenado de matriz");
+		
+		for (int i = 0; i < mat1.length; i++){
+			for (int j = 0; j < mat1.length; j++){
+				System.out.print("Ingrese el valor [ " + (i+1) + "] [" + (j+1) + " ]: ");
+				mat1[i][j] = input.nextInt();
+			}
+			
+		}
+		
+
+	}
+	
+	public static void sumar (int[][] M1, int[][] M2, int[][] M3 ) {
+		
+		for (int i = 0; i < M2.length; i++) {
+			for (int j = 0; j < M2.length; j++) {
+				M3[i][j]= M1[i][j] + M2[i][j];
+			}
+		}
+	}
+	
+	public static void imp (int[][] M3) {
+		for (int i = 0; i < M3.length; i++) {
+			for (int j = 0; j < M3.length; j++) {
+				System.out.print(M3[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+	
+}
